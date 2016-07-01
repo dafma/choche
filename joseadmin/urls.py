0ad8ba4a802be7581c8e3c68aa3dae76b8910b54
seader <seader@gmail.com>
2016-06-30 19:32:34 -0500

    form paquete

--- joseadmin/urls.py
+++ joseadmin/urls.py
@@ -2,7 +2,7 @@ __author__ = 'mrk2'
 from django.conf.urls import url
 from django.contrib import admin
 from . import views
-from .views import ReservacionList, ClientesList, PaqueteList
+from .views import ReservacionList, ClientesList, PaqueteList, PaqueteCreation, PaqueteUpdate,PaqueteEdit
 
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
@@ -10,6 +10,13 @@ urlpatterns = [
     url(r'^reservaciones/$', ReservacionList.as_view(), name='reservaciones'),
     url(r'^clientes/$', ClientesList.as_view(), name='clientes'),
     url(r'^paquete/$', PaqueteList.as_view(), name='paquetes'),
+
+
+    # crud paquete
+    url(r'^nuevo_paquete$', PaqueteCreation.as_view(), name='paqueteNew'),
+
+
+
 
 
 ]
--- joseadmin/views.py
+++ joseadmin/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from app.models import *
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 
 # Create your views here.
 
@@ -17,3 +19,18 @@ class PaqueteList(ListView):
     model = Paquete
     template_name = 'paquete-admin.html'
 
+class PaqueteCreation(CreateView):
+    model = Paquete
+    template_name = "paquetes/crear_paquete.html"
+    success_url = reverse_lazy('administrador:paquetes')
+    fields = ['nombre', 'personas', 'sillas', 'cubresillas', 'mesas', 'manteles', 'cubremanteles', 'juego_loza', 'precio']
+
+class PaqueteUpdate(UpdateView):
+    model = Paquete
+    success_url = reverse_lazy('administrador:paquetes')
+    fields = ['nombre', 'personas', 'sillas', 'cubresillas', 'mesas', 'manteles', 'cubremanteles', 'juego_loza', 'precio']
+
+
+class PaqueteEdit(DeleteView):
+    model = DeleteView
+    success_url = reverse_lazy('administrador:paquetes')
